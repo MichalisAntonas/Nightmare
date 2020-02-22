9832a7cce569152380714f62ef4f17a89ad85b8a
Tilman Raendchen <TRaendchen@outlook.com>
2020-02-22 13:17:54 +0000

    Clean up Zombie Script

--- Nightmare/Assets/Scripts/ZombieAI.cs
+++ Nightmare/Assets/Scripts/ZombieAI.cs
@@ -4,75 +4,60 @@ using UnityEngine;
 
 public class ZombieAI : MonoBehaviour
 {
-
     public GameObject thePlayer;
-    public GameObject theEnemy;
-    public float enemySpeed = 0.01f;
-    public bool attackTrigger = false;
+    public float enemySpeed = .02f;
+    bool attackTrigger = false;
     public bool isAttacking = false;
-    public AudioSource hurtSound1;
-    public AudioSource hurtSound2;
-    public AudioSource hurtSound3;
-    public int hurtGen;
+    public AudioSource[] hurtSounds;
     public GameObject theFlash;
- 
+
+    void Start()
+    {
+        this.GetComponentInChildren<Animation>().wrapMode = WrapMode.Loop;
+    }
 
    //Zombie looks towards Player and follows him
     void Update()
     {
         transform.LookAt(thePlayer.transform);
-        if(attackTrigger==false)
+        transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, enemySpeed);
+        
+        if (attackTrigger && !isAttacking)
         {
-            enemySpeed = 0.02f;
-            theEnemy.GetComponent<Animation>().Play("Z_Walk_InPlace");
-            transform.position = Vector3.MoveTowards(transform.position, thePlayer.transform.position, enemySpeed);
-        }
-        if (attackTrigger == true && isAttacking == false)
-        {
-            enemySpeed = 0;
-            theEnemy.GetComponent<Animation>().Play("Z_Attack");
+            this.GetComponentInChildren<Animation>().Play("Z_Attack");
             StartCoroutine(InflictDamage());
         }
-       
-
     }
+
     //Zombie attacks if its in Players Collider
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider collider)
     {
-
-        attackTrigger = true;
-
+        if (collider.gameObject.transform.parent.gameObject.tag == "MainCamera")
+        {
+            attackTrigger = true;
+            enemySpeed = 0;
+        }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.transform.parent.gameObject.tag == "MainCamera")
+        {
+            enemySpeed = 0.02f;
+            this.GetComponentInChildren<Animation>().Play("Z_Walk_InPlace");
+            attackTrigger = false;
+        }
     }
 
-    void OnTriggerExit(Collider other)
-    {
-
-        attackTrigger = false;
-    }
     IEnumerator InflictDamage()
     {
         isAttacking = true;
-        hurtGen = Random.Range(1, 4);
-        if (hurtGen == 1)
-        {
-            hurtSound1.Play();
-        }
-        if (hurtGen == 2)
-        {
-            hurtSound2.Play();
-        }
-        if (hurtGen == 3)
-        {
-            hurtSound3.Play();
-        }
-        
+        hurtSounds[Random.Range(0, 3)].Play();
         
         theFlash.SetActive(true);
         yield return new WaitForSeconds(0.1f);
         theFlash.SetActive(false);
         yield return new WaitForSeconds(1f);
         GlobalHealth.currentHealth -= 5;
-        
 
         yield return new WaitForSeconds(0.09f);
         isAttacking = false;
