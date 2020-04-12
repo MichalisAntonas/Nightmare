49c1e4d4e5bf40f8e61508d9dd73eb3749be8475
Tilman Raendchen <TRaendchen@outlook.com>
2020-04-12 19:00:30 +0100

    Implement Jumpscare Triggers

---
+++ Assets/Prefabs/Triggers/Jumpscare/Trigger_Jumpscare.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_Jumpscare : MonoBehaviour
+{
+    [Tooltip("The percentage chance for this trigger to activate when walked into. This object will destroy itself when colliding with the player, whether or not the trigger was activated.")]
+    public float Chance = 100;
+    public List<AudioClip> AudioClips;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+            return;
+
+        if (Random.value * 100 <= this.Chance)
+            other.GetComponent<AudioSource>().PlayOneShot(this.AudioClips[Random.Range(0, this.AudioClips.Count)], .5f);
+
+        Destroy(this.gameObject);
+    }
+}
