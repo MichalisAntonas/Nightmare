915d57d83a7075b84c2709d49b4f2fc9b611d352
Michalis <michalis-a@hotmail.co.uk>
2021-02-26 21:07:23 +0000

    Added Tutorial Text

--- Assets/Scripts/Sequences/AOpening.cs
+++ Assets/Scripts/Sequences/AOpening.cs
@@ -9,6 +9,7 @@ public class AOpening : MonoBehaviour
     public GameObject ThePlayer;
     public GameObject FadeScreenIn;
     public GameObject TextBox;
+    public GameObject Controls;
     void Start()
     {
         //ThePlayer.GetComponent<FirstPersonController>().enabled = false;
@@ -21,8 +22,14 @@ public class AOpening : MonoBehaviour
         FadeScreenIn.SetActive(false);
        
         TextBox.GetComponent<Text>().text = "Where am I?";
+        Controls.GetComponent<Text>().text = "Movement - W,A,S,D\nJump - Space\nRun - Shift\nPick Up Itmes - E\nFire - Left Click";
         yield return new WaitForSeconds(7);
         TextBox.GetComponent<Text>().text = "";
-       // ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+        Controls.GetComponent<Text>().text = "";
+
+
+        yield return new WaitForSeconds(7);
+        
+        // ThePlayer.GetComponent<FirstPersonController>().enabled = true;
     }
 }
